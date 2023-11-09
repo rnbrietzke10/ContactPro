83962e7c17d746663feb54f2745cce55217e577c
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-09 09:58:54 -0600

    Add models for category, and contact. Add virtuals to create join table. Add list of states as an Enum.

--- ContactPro/Program.cs
+++ ContactPro/Program.cs
@@ -1,8 +1,10 @@
 using ContactPro.Data;
+using ContactPro.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -12,7 +14,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
 	.AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
