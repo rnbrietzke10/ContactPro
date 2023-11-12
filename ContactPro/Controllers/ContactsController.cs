de4a21e169dd43bbfc422794bf3fb5bb4d8e9d6d
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-12 07:49:43 -0600

    write code to filter by category

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -33,7 +33,7 @@ namespace ContactPro.Controllers
 
         // GET: Contacts
         [Authorize]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int categoryId)
         {
            List<Contact> contacts = new List<Contact>();
             string appUserId = _userManager.GetUserId(User);
@@ -45,9 +45,18 @@ namespace ContactPro.Controllers
 
             var categories = appUser.Categories;
 
-            contacts = appUser.Contacts.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
 
-            ViewData["CategoryId"] = new SelectList(categories, "Id", "Name");
+            if(categoryId == 0)
+            {
+				contacts = appUser.Contacts.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
+			} else
+            {
+                contacts = appUser.Categories.FirstOrDefault(c => c.Id == categoryId)!.Contacts.OrderBy(c => c.LastName).ThenBy(c => c.FirstName).ToList();
+            }
+
+            
+
+            ViewData["CategoryId"] = new SelectList(categories, "Id", "Name", categoryId);
 
             return View(contacts);
         }
