f06d411e18d83348268063e699a362ff10dcd2bd
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-14 10:16:17 -0600

    Implement search functionality for contacts view

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -33,7 +33,7 @@ namespace ContactPro.Controllers
 
         // GET: Contacts
         [Authorize]
-        public async Task<IActionResult> Index(int categoryId)
+        public IActionResult Index(int categoryId)
         {
            List<Contact> contacts = new List<Contact>();
             string appUserId = _userManager.GetUserId(User);
@@ -61,8 +61,37 @@ namespace ContactPro.Controllers
             return View(contacts);
         }
 
+		[Authorize]
+		public IActionResult SearchContacts(string searchString)
+        {
+            string appUserId = _userManager.GetUserId(User);
+            var contacts = new List<Contact>();
+            
+            AppUser appUser = _context.Users
+                                      .Include(c => c.Contacts)
+                                      .ThenInclude(c => c.Categories)
+                                      .FirstOrDefault(u => u.Id == appUserId)!;
+            if (String.IsNullOrEmpty(searchString))
+            {
+                contacts = appUser.Contacts
+                                  .OrderBy(c => c.LastName)
+                                  .ThenBy(c => c.FirstName)
+                                  .ToList();
+
+			} else
+            {
+				contacts = appUser.Contacts.Where(c => c.FullName!.ToLower().Contains(searchString.ToLower()))
+								 .OrderBy(c => c.LastName)
+								 .ThenBy(c => c.FirstName)
+								 .ToList();
+			}
+            ViewData["CategoryId"] = new SelectList(appUser.Categories, "Id", "Name", 0);
+
+			return View(nameof(Index),contacts);
+        }
+
         // GET: Contacts/Details/5
-        [Authorize]
+      [Authorize]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.Contacts == null)
