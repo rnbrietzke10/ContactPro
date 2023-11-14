c016e0de8cc077a7a723990ecdca3416d0aaa131
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-14 11:17:30 -0600

    Add states list to Edit form

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -169,13 +169,21 @@ namespace ContactPro.Controllers
                 return NotFound();
             }
 
-            var contact = await _context.Contacts.FindAsync(id);
+            string appUserId = _userManager.GetUserId(User);
+
+
+            // var contact = await _context.Contacts.FindAsync(id);
+            var contact = await _context.Contacts.Where(c => c.Id == id && c.AppUserId == appUserId)
+                                                 .FirstOrDefaultAsync();
             if (contact == null)
             {
                 return NotFound();
             }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", contact.AppUserId);
-            return View(contact);
+
+			ViewData["StatesList"] = new SelectList(Enum.GetValues(typeof(States)).Cast<States>().ToList());
+			ViewData["CategoryList"] = new MultiSelectList(await _addressBookService.GetUserCategoriesAsync(appUserId), "Id", "Name");
+
+			return View(contact);
         }
 
         // POST: Contacts/Edit/5
