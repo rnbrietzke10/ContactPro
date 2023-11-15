9ca5795beb27390f1ad62968dd27cb6ef910c285
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-15 13:18:51 -0600

    create a new model for EmailData, create a new class for EmailContactViewModel

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using ContactPro.Data;
 using ContactPro.Models;
+using ContactPro.Models.ViewModels;
 using ContactPro.Enums;
 using ContactPro.Services.Interfaces;
 using ContactPro.Services;
@@ -112,9 +113,27 @@ namespace ContactPro.Controllers
 
 
         [Authorize]
-        public IActionResult EmailContact(int contactId)
+        public async Task<IActionResult> EmailContact(int id)
         {
-            return View();
+            string appUserId = _userManager.GetUserId(User);
+            Contact contact = await _context.Contacts.Where(c => c.Id == id && c.AppUserId == appUserId).FirstOrDefaultAsync();
+
+            if(contact == null)
+            {
+                return NotFound();
+            }
+            EmailData emailData = new EmailData()
+            {
+                EmailAddress = contact.Email!,
+                FirstName = contact.FirstName,
+                LastName = contact.LastName
+            };
+            EmailContactViewModel model = new EmailContactViewModel()
+            {
+                Contact = contact,
+                EmailData = emailData
+            };
+            return View(model);
         }
 
 		// GET: Contacts/Create
