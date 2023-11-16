fe8fa4ccee068a27f9a364ce7a4b5a54ffdb9be3
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-16 11:42:06 -0600

    remove and edit some scaffolded code, fix a security issue to filter by signed-in user

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -87,17 +87,22 @@ namespace ContactPro.Controllers
 		[Authorize]
 		public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.Categories == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var category = await _context.Categories.FindAsync(id);
+            string appUserId = _userManager.GetUserId(User);
+
+
+
+            var category = await _context.Categories.Where(c => c.Id == id && c.AppUserId == appUserId)
+                                                    .FirstOrDefaultAsync();
             if (category == null)
             {
                 return NotFound();
             }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
+            
             return View(category);
         }
 
