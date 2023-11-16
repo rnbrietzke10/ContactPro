d33bbcd4dc78eba089c861886a2a14aaf086c95a
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-16 11:15:04 -0600

    remove unnecessary fields and change some onscreen text, fix the code to only allow deletion of contacts you own

--- ContactPro/Controllers/ContactsController.cs
+++ ContactPro/Controllers/ContactsController.cs
@@ -317,9 +317,10 @@ namespace ContactPro.Controllers
                 return NotFound();
             }
 
+
+            string appUserId = _userManager.GetUserId(User);
             var contact = await _context.Contacts
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
+                                        .FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
             if (contact == null)
             {
                 return NotFound();
@@ -333,17 +334,20 @@ namespace ContactPro.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Contacts == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Contacts'  is null.");
-            }
-            var contact = await _context.Contacts.FindAsync(id);
+
+			string appUserId = _userManager.GetUserId(User);
+
+
+            var contact = await _context.Contacts.FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
+
             if (contact != null)
             {
                 _context.Contacts.Remove(contact);
-            }
+				await _context.SaveChangesAsync();
+			}
             
-            await _context.SaveChangesAsync();
+           
             return RedirectToAction(nameof(Index));
         }
 
