801309c8ad529b23537f265df4eb75a02701d7e8
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-16 12:18:55 -0600

    build the EmailCategory action, create and update EmailCategory view using code from our template, create EmailCategoryViewModel

--- ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/Controllers/CategoriesController.cs
@@ -38,6 +38,32 @@ namespace ContactPro.Controllers
             return View(categories);
         }
 
+        [Authorize]
+        public async Task<IActionResult> EmailCategory(int id)
+        {
+            string appUserId = _userManager.GetUserId(User);
+            Category? category = await _context.Categories.Include(c => c.Contacts)
+                                                         .FirstOrDefaultAsync(c => c.Id == id && c.AppUserId == appUserId);
+
+            List<string> emails = category.Contacts.Select(c => c.Email).ToList();
+
+            EmailData emailData = new EmailData()
+            {
+                GroupName = category.Name,
+                EmailAddress = String.Join(";", emails), 
+                Subject = $"Group Message: {category.Name}"
+            };
+
+            EmailCategoryViewModel model = new EmailCategoryViewModel()
+            {
+                Contacts = category.Contacts.ToList(),
+                EmailData = emailData
+            };
+
+
+			return View(model);
+        }
+
 		// GET: Categories/Details/5
 		[Authorize]
 		public async Task<IActionResult> Details(int? id)
